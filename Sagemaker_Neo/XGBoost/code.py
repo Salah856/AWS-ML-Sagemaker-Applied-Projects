e65c735712d528d44d735046e0f5a2ed935f2225
Salah Othman <salah_elhossiny856@yahoo.com>
2021-06-08 11:38:02 +0200

    Update code.py

--- Sagemaker_Neo/XGBoost/code.py
+++ Sagemaker_Neo/XGBoost/code.py
@@ -1,1 +1,10 @@
 
+from pathlib import Path
+import boto3
+
+for p in ['raw_data', 'training_data', 'validation_data']:
+    Path(p).mkdir(exist_ok=True)
+
+s3 = boto3.client('s3')
+s3.download_file('sagemaker-sample-files', 'datasets/tabular/uci_abalone/abalone.libsvm', 'raw_data/abalone')
+
